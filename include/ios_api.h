38aeb982b3bf207778a56489bd4d604a342c1cd4
PandiPanda69 <thedestiny@SNuX-Vostro-3550.(none)>
2012-01-27 11:06:51 +0100

    Nouvelle API corrigeant des oublis...

--- include/ios_api.h
+++ include/ios_api.h
@@ -1,49 +1,48 @@
 #ifndef __IOS_API_H__
 #define __IOS_API_H__
 
+#include "drv_api.h"
+
+/**
+Initialise l'ios avant utilisation
+\return IOS_OK si tout est ok
+*/
+int ios_init();
+
+/**
+Libère l'ios de la mémoire
+*/
+void ios_release();
+
 /**
 Charge un nouveau driver afin d'exploiter ses fonctionnalités
 \param	driver_name	Le nom du fichier .so à charger
-\return	Le majeur si tout est ok ou une erreur < 0 sinon
+\return	Le majeur si tout est ok ou une erreur (IOS_UNKNOWN_DRIVER, IOS_INVALID_DRIVER, IOS_TOO_MANY_DRIVER_PLUGGED)
 */
 int ios_install_driver( const char* driver_name );
 
 /**
 Désinstalle un driver chargé en mémoire en cloturant tous les fd si nécessaire
 \param	major	Le majeur retourné lors de l'installation
-\return IOS_OK si tout est ok, ou IOS_UNKNOWN_DRIVER si le driver est inconnu
 */
-int ios_uninstall_driver( int major );
+void ios_uninstall_driver( int major );
 
 /**
 Ajoute un nouveau périphérique rattaché à un driver  en lui associant un nom
 \param	major		Le majeur du driver
 	id		L'identifiant unique du périphérique
-	device_name	Le nom qui sera associé au périphérique
-\return IOS_OK si tout est ok, IOS_ID_ALREADY_ADDED si le device existe déjà, ou IOS_NAME_ALREADY_ASSIGNED si le nom est déjà pris
+	ip_address	L'adresse IP du concentrateur/du capteur associé à cet id
+	port		Le port sur lequel se connecter au périphérique distant
+\return Le descripteur si tout est ok, IOS_ID_ALREADY_ADDED si le device existe déjà, ou IOS_INVALID_PORT si le port est douteux
 */
-int ios_add_device( int major, int id, const char* device_name );
+int ios_add_device( int major, int id, const char* ip_address, unsigned int port );
 
 /**
 Enlève un périphérique déjà ajouté grâce à son nom
-\param	device_name	Le nom associé au périphérique
+\param	fd	Le descripteur du périphérique
 \return	IOS_OK si tout est ok, IOS_UNKNOWN_DEVICE si le périphérique est introuvable
 */
-int ios_remove_device( const char* device_name );
-
-/**
-Ouvre un périphérique
-\param	device_name	Le nom associé au périphérique
-\return	Le descripteur de fichier du périphérique ou IOS_UNKNOWN_DEVICE si le périphérique est introuvable ou IOS_ERROR si l'erreur est critique
-*/
-int ios_open( const char* device_name );
-
-/**
-Ferme un périphérique
-\param	fd	Le descripteur de fichier
-\return IOS_OK si tout est ok, ou IOS_UNKNOWN_FD si le descripteur est inconnu
-*/
-int ios_close( int fd );
+int ios_remove_device( int fd );
 
 /**
 Lit une donne en particulier provenant d'un périphérique
