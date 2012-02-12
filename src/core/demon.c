429253e17fde138be333e9324cfd851c3b7e3499
rclaude <charlesalbert.claude@gmail.com>
2012-02-12 17:01:25 +0100

    libxml2 is just so boring

--- src/core/demon.c
+++ src/core/demon.c
@@ -5,44 +5,57 @@
 #include <libxml/tree.h>
 #include <libxml/parser.h>
 
+#include "XMLParser.h"
 #include "ios_api.h"
 #include "engine.h"
 #include "demon.h"
 
+
 int main(){
-	int drv;
-	int fd[3];
-
-	ios_init();
-
-	drv = ios_install_driver( "mon_driver.so.1.0.1", "127.0.0.1", 8080 );
-	if( drv < 0 )
-	{
-		ios_release();
-		return drv;
-	}
 
 	xmlDocPtr capteurs_doc;
-	xmlNodePtr node_capteur;
-	xmlNodePtr node_infos;
+	xmlNodePtr document_node;
+	xmlNodePtr driver_node;
+	xmlNodePtr capteur_node;
 	
-	capteurs_doc = xmlParseFile("capteurs.xml");
-	node_capteur = capteurs_doc->children;
+	capteurs_doc = xmlParseFile("drivers.xml");
+	perror("xmlParseFile");
+	document_node = capteurs_doc->children;
+
+	/* get the number of drivers and of capteurs */
+	int drv;
+	int fd[9];
+
+	/* init, start drivers and add all the capteurs */
+	ios_init();
+	int driver_counter = 0;	
+	for( 	driver_node = document_node->children;
+		driver_node != NULL;
+		driver_node = driver_node->next)
 	{
-		printf("LOOP\n");
-		int loop_counter = 0;
-		printf("node_capteur %s\n",node_capteur->name);
-		for( 	node_infos = node_capteur->children;
-			node_infos != NULL;
-			node_infos = node_infos->next)
+		//drv = ios_install_driver( "mon_driver.so.1.0.1", "127.0.0.1", 8080 );
+		if( drv < 0 )
+		{	
+			ios_release();
+			return drv;
+		}
+		int capteur_counter = 0;
+		printf("%s,%i\n",driver_node->name,driver_node->type);
+		for( 	capteur_node = document_node->children;
+			capteur_node != NULL;
+			capteur_node = capteur_node->next)
 		{
-			printf("\t%s\n",node_infos->name);
-			xmlChar* id = xmlGetProp(node_capteur,(const xmlChar*)"id");
-			fd[loop_counter] = ios_add_device( drv,(int) id );
-			loop_counter++;
+			printf("\t%s\n",capteur_node->name);
+			//xmlChar* id = xmlGetProp(document_node,(const xmlChar*)"id");
+			//fd[capteur_counter] = ios_add_device( drv,(int) id );
+			capteur_counter++;
 		}
 		printf("end LOOP");
+		drv[driver_counter]++;
 	}
+	if(driver_node == NULL)
+		printf("no child\n ");
 	xmlFreeDoc(capteurs_doc);
 	return 0;
 }
+
