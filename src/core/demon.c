5e2bcd9e95b64683aa001787bfb049da19bf2794
rclaude <charlesalbert.claude@gmail.com>
2012-02-13 15:08:48 +0100

    First functional version with the test driver read xml, start driver, add sensors TODO: test with the enocean driver now!

--- src/core/demon.c
+++ src/core/demon.c
@@ -10,51 +10,60 @@
 #include "engine.h"
 #include "demon.h"
 
+xmlNodePtr next_XML_ELEMENT_NODE(xmlNodePtr node){
+	node = node->next;
+	while(node != NULL && node->type != XML_ELEMENT_NODE){
+		node = node->next;
+	}
+	return node;
+}
 
 int main(){
 
 	xmlDocPtr capteurs_doc;
-	xmlNodePtr document_node;
+	xmlNodePtr dtd_node;
+	xmlNodePtr drivers_node;
 	xmlNodePtr driver_node;
 	xmlNodePtr capteur_node;
 	
 	capteurs_doc = xmlParseFile("drivers.xml");
 	perror("xmlParseFile");
-	document_node = capteurs_doc->children;
+	/* the first node of the file is the DTD node */
+	dtd_node = capteurs_doc->children;
 
 	/* get the number of drivers and of capteurs */
-	int drv;
+	int drv = 1;
 	int fd[9];
 
 	/* init, start drivers and add all the capteurs */
 	ios_init();
 	int driver_counter = 0;	
-	for( 	driver_node = document_node->children;
+	drivers_node = dtd_node->next;
+	for( 	driver_node = drivers_node->children->next;
 		driver_node != NULL;
-		driver_node = driver_node->next)
+		driver_node = next_XML_ELEMENT_NODE(driver_node))
 	{
-		//drv = ios_install_driver( "mon_driver.so.1.0.1", "127.0.0.1", 8080 );
+		printf("CORE/ driver - %s,%i\n",driver_node->name,driver_node->type);
+		xmlChar* drv_so_name = xmlGetProp(driver_node,(const xmlChar*)"so");
+		drv = ios_install_driver( (char*)drv_so_name, "127.0.0.1", 8080 );
+		xmlFree(drv_so_name);
 		if( drv < 0 )
 		{	
 			ios_release();
 			return drv;
 		}
 		int capteur_counter = 0;
-		printf("%s,%i\n",driver_node->name,driver_node->type);
-		for( 	capteur_node = document_node->children;
+		for( 	capteur_node = driver_node->children->next;
 			capteur_node != NULL;
-			capteur_node = capteur_node->next)
+			capteur_node = next_XML_ELEMENT_NODE(capteur_node))
 		{
-			printf("\t%s\n",capteur_node->name);
-			//xmlChar* id = xmlGetProp(document_node,(const xmlChar*)"id");
-			//fd[capteur_counter] = ios_add_device( drv,(int) id );
+			printf("CORE/\tcapteur - %s,%i\n",capteur_node->name,capteur_node->type);
+			xmlChar* id = xmlGetProp(drivers_node,(const xmlChar*)"id");
+			fd[capteur_counter] = ios_add_device( drv,(int) id );
 			capteur_counter++;
 		}
-		printf("end LOOP");
-		drv[driver_counter]++;
+		driver_counter++;
 	}
-	if(driver_node == NULL)
-		printf("no child\n ");
 	xmlFreeDoc(capteurs_doc);
 	return 0;
 }
