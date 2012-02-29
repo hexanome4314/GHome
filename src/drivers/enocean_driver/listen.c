40ab3b233f6d2289adafbc0bb38f637ec6b8911a
PierreLu <pierrelule@gmail.com>
2012-02-29 19:48:00 +0100

    correction reception 4BS

--- src/drivers/enocean_driver/listen.c
+++ src/drivers/enocean_driver/listen.c
@@ -283,30 +283,25 @@ void _interpretAndSend4BS(enocean_data_structure* a_RPS_message, int* msgq_id){
 	id += a_RPS_message->ID_BYTE0;
 
 	// Info du capteur de type temperature.
-	msgTemp.flag_value = DRV_FIELD_TEMPERATURE;
 	msg2.flag_value = DRV_FIELD_LIGHTING;
 	msg3.flag_value = DRV_FIELD_VOLTAGE;
+	msg2.msg_type = DRV_MSG_TYPE;
+	msg3.msg_type = DRV_MSG_TYPE;
 
-	// On calcule la temperature du capteur.
-	msgTemp.value = a_RPS_message->DATA_BYTE1 * 40/255;
-	msgTemp.id_sensor = id;
-
-	msg2.value = a_RPS_message->DATA_BYTE2 * 510/255;
+	msg2.value = a_RPS_message->DATA_BYTE2 * 510.0/256.0;
 	msg2.id_sensor = id;
 
-	msg3.value = a_RPS_message->DATA_BYTE3 * 5.1/255;
+	msg3.value = a_RPS_message->DATA_BYTE3 * 5.1/256.0;
 	msg3.id_sensor = id;
 
 	if (LOG)
 	{
-		printf("Capteur : %X Temperature : %f !!!!\n", id, msgTemp.value);
 		printf("Capteur : %X Luminosite : %f !!!!\n", id, msg2.value);
 		printf("Capteur : %X Voltage : %f !!!!\n", id, msg3.value);
 	}
 
-	resp = msgsnd( (msgqnum_t)msgq_id, (const void*) &msgTemp, sizeof(struct msg_drv_notify) - sizeof(long), 0 );
-	resp = msgsnd( (msgqnum_t)msgq_id, (const void*) &msg2, sizeof(struct msg_drv_notify) - sizeof(long), 0 );
-	resp = msgsnd( (msgqnum_t)msgq_id, (const void*) &msg3, sizeof(struct msg_drv_notify) - sizeof(long), 0 );
+	resp = msgsnd( *msgq_id, (const void*) &msg2, sizeof(struct msg_drv_notify) - sizeof(long), 0 );
+	resp = msgsnd( *msgq_id, (const void*) &msg3, sizeof(struct msg_drv_notify) - sizeof(long), 0 );
 }
 
 /********************************************* PUBLICS FUNCTIONS */
@@ -464,5 +459,3 @@ void interpretAndSend(int* msgq_id){
 		}
 	}/* end INTERPRET&SEND THREAD LOOP */
 }
-
-
